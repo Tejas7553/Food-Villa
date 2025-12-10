747ff3e8290163d3443fc5371c790ea3ee2c6920
Tejas7553 <jadhav.tejas7553@gmail.com>
2025-12-10 20:18:19 +0530

    Demo2

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
+//JSX => Raect.createElement => Object => HTML(DOM)
 
+//Below is the React element
+const Title = () => (
+  <h1 id="titel" key="h1">
+    Namskar Reactt by Babel
+  </h1>
+);
+
+//React Component
+// Functional Component
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/* Component Composition */}
+      {<Title />}  
+      <h2>Namste React Functional Component</h2>
+      <h2>This is an H2 tag</h2>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<HeaderComponent />);
 
 // JavaScript Code :  Manipulating the HTML DOM using Javscript
 /*
@@ -9,15 +32,14 @@ const heading = document.createElement("h1");
 heading.innerHTML = "Hello World from JavaScript!"
 const root = document.getElementById("root");
 root.appendChild(heading);
-*/ 
- 
+*/
+
 // React Code : Manipulating the HTML DOM using React
 /*
 const heading = React.createElement("h1", {}, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 */
-
 
 // creating Nested React Elements Object: Here h1 is an React Object not HTML Element
 /*
@@ -33,7 +55,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // Passing React Element(object h1) inside root
 root.render(heading);
 */
-
 
 // creating Nested React Elements Object
 /**
@@ -81,7 +102,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(App());
 */
 
-
 // creating Sibling React Elements Object
 /**
  * <div id="parent">
@@ -119,7 +139,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 */
 
-
 // creating Sibling React Elements Object
 /**
  * <div id="parent">
@@ -151,7 +170,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 */
 
-
 // creating Sibling React Elements Object
 /**
  * <div id="parent" style="background: skyblue;">
@@ -167,26 +185,3 @@ root.render(parent);
  *
  * ReactElement(Object) => HTML(Browser Understands)
  */
-
-const child1 = React.createElement("div", { id: "child1" }, [
-  React.createElement("h1", {}, "I am an h1 tag"),
-  React.createElement("h2", {}, "I am an h2 tag"),
-]);
-const child2 = React.createElement("div", { id: "child2" }, [
-  React.createElement("h1", {}, "I am an h1 tag"),
-  React.createElement("h2", {}, "I am an h2 tag"),
-]);
-
-const parent = React.createElement(
-  "div",
-  {
-    id: "parent",
-    style: {
-      background: "skyblue",
-    },
-  },
-  [child1, child2]
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
